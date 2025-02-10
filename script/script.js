099d0182d7184fdc800819c8b272eb86d832d336
alemanhaf <fernando.silva09@aluno.ifce.edu.br>
2025-02-09 23:45:37 -0300

    recebendo uma nova data, ano e mes atual

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
 const days = document.querySelector(".dates");
 const current_date = document.querySelector(".current-date");
 const icons_btn = document.querySelectorAll(".icons span");
+
+//Recebendo uma nova data, ano e mês atual
+
+let date = new Date(), 
+current_year = date.getFullYear(),
+current_month = date.getMonth();
+
