56d4801d67fac6a7bd8573a51827d62e86f6c8a1
ViniMesquita.dev <167264281+ViniMesquitaa@users.noreply.github.com>
2025-02-10 23:24:52 -0300

    add vetor with all months

--- script/script.js
+++ script/script.js
@@ -4,6 +4,9 @@ const icons_btn = document.querySelectorAll(".icons span");
 
 //Recebendo uma nova data, ano e mês atual
 
+const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
+              "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
 let date = new Date(), 
 current_year = date.getFullYear(),
 current_month = date.getMonth();
