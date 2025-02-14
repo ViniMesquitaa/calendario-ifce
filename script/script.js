94ad7c1b0fff21a4b079af827011670904f90310
Nicolas <lima.nicolas62@aluno.ifce.edu.br>
2025-02-14 13:30:40 -0300

    dia atual n fica colorido quando coloco task

--- script/script.js
+++ script/script.js
@@ -24,24 +24,20 @@ let date = new Date(),
   current_year = date.getFullYear(),
   current_month = date.getMonth();
 
-// Função para renderizar o calendário
+// Função para renderizar o calendário com todos os dias 
 function renderCalendar() {
   let firstDayMonth = new Date(current_year, current_month, 1).getDay();
   let lastDateMonth = new Date(current_year, current_month + 1, 0).getDate();
-  let lastDayOfMonth = new Date(
-    current_year,
-    current_month,
-    lastDateMonth
-  ).getDay();
+  let lastDayOfMonth = new Date(current_year, current_month, lastDateMonth).getDay();
   let lastDateLastMonth = new Date(current_year, current_month, 0).getDate();
   let daysMonth = "";
 
-  // Preencher os dias do mês anterior (se necessário)
+  // Preencher os dias do mês anterior pra ficar calendario completo na semana
   for (let i = firstDayMonth; i > 0; i--) {
     daysMonth += `<li class="last-days">${lastDateLastMonth - i + 1}</li>`;
   }
 
-  // Preencher os dias do mês atual
+  // Preencher os dias do mês atual na semaana
   for (let i = 1; i <= lastDateMonth; i++) {
     let today =
       i === date.getDate() &&
@@ -50,13 +46,12 @@ function renderCalendar() {
         ? "today"
         : "";
     let key = `${current_year}-${current_month + 1}-${i}`;
-    let taskClass =
-      tasksStorage[key] && tasksStorage[key].length > 0 ? "task-day" : "";
+    let taskClass = tasksStorage[key] && tasksStorage[key].length > 0 && today === "" ? "task-day" : "";
 
+    // Não muda a cor do dia atual, apenas os outros dias com tarefas
     daysMonth += `<li class="${today} ${taskClass}" onclick="selectDay(${i})">${i}</li>`;
   }
 
-  // Preencher os dias do mês seguinte (se necessário)
   for (let i = 1; i <= 6 - lastDayOfMonth; i++) {
     daysMonth += `<li class="last-days">${i}</li>`;
   }
@@ -65,7 +60,7 @@ function renderCalendar() {
   days.innerHTML = daysMonth;
 }
 
-// Função para selecionar o dia e exibir as tarefas
+// Função para selecionar o dia e exibir as tasks 
 function selectDay(day) {
   let today = new Date();
   let isPastDate =
@@ -101,7 +96,7 @@ function showTasks() {
     <ul class="task-list">
       ${tasks
         .map(
-          (task, index) => `
+          (task, index) => `        
         <li>
           ${task} 
           <button onclick="deleteTask(${index})">❌</button>
@@ -113,7 +108,6 @@ function showTasks() {
 
   document.getElementById("taskInput").focus();
 }
-
 
 // Função para adicionar uma tarefa
 function addTask() {
@@ -137,7 +131,6 @@ function addTask() {
   }
 }
 
-
 // Função para excluir uma tarefa
 function deleteTask(index) {
   if (!selectedDate) return;
@@ -157,7 +150,7 @@ function loadTodayTasks() {
   selectedDate = `${current_year}-${current_month + 1}-${date.getDate()}`;
   showTasks();
 }
-// Função para mudar o mês
+
 function navigate(direction) {
   current_month += direction;
 
@@ -177,8 +170,7 @@ function navigate(direction) {
 renderCalendar();
 loadTodayTasks();
 
-
-// Função do botão "Hoje"
+// Função do botão pro refresh no footer
 const todayButton = document.getElementById("today-btn");
 
 todayButton.addEventListener("click", function() {
@@ -187,18 +179,6 @@ todayButton.addEventListener("click", function() {
   current_month = date.getMonth(); // Atualiza o mês
   renderCalendar(); // Atualiza o calendário para o mês atual
 });
-
-// Função para adicionar tarefas ao dia específico
-function addTaskCurrent(day) {
-  let taskListContainer = document.getElementById("show-tasks");
-  taskListContainer.innerHTML = "";
-
-  let header = document.createElement("div");
-  header.id = `header-${day}`;
-  header.className = "header-tasks";
-
-  
-}
 
 function updateTime() {
   const timeElement = document.getElementById('current-time');
@@ -210,7 +190,6 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 updateTime();
-
 
 function updateDate() {
   const dateElement = document.getElementById('current-date');
